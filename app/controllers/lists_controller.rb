c6d8e75c1956f0124e784d00d181c8334f6328ef
EC2 Default User <107122362+motikinako@users.noreply.github.com>
2022-11-21 13:20:03 +0000

    edit

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -9,19 +9,29 @@ class ListsController < ApplicationController
     # 3. データをデータベースに保存するためのsaveメソッド実行
     list.save
     # 4. トップ画面へリダイレクト
-    redirect_to '/top'
+    redirect_to list_path(list.id)
     end
 
 
   def index
-    @list = List.
+   @lists = List.all
   end
 
   def show
+    @list = List.find(params[:id])
   end
 
   def edit
+    @list = List.find(params[:id])
   end
+
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to list_path(list.id)
+  end
+
+
 
     private
   # ストロングパラメータ
